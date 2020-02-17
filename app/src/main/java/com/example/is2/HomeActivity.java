525cd3a5025da6d450fcc6c81ea06e04d3ecfe3d
filippodev318 <fili.perna@gmail.com>
2020-02-17 15:45:21 +0100

    Uncommented super.onBackPressed

--- app/src/main/java/com/example/is2/HomeActivity.java
+++ app/src/main/java/com/example/is2/HomeActivity.java
@@ -14,7 +14,7 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        // super.onBackPressed(); commented this line in order to disable back press
+         super.onBackPressed(); //commented this line in order to disable back press
         //Write your code here
         //Toast.makeText(getApplicationContext(), "Back press disabled!", Toast.LENGTH_SHORT).show();
     }
