507215ed770db6436f0b95a16bec17cb00cc0d9e
filippodev318 <fili.perna@gmail.com>
2020-02-21 09:59:57 +0100

    Google Maps in Around Me

--- app/src/main/java/com/example/is2/ui/aroundme/AroundmeFragment.java
+++ app/src/main/java/com/example/is2/ui/aroundme/AroundmeFragment.java
@@ -9,19 +9,41 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.is2.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class AroundmeFragment extends Fragment {
+public class AroundmeFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    MapView mMapView;
+    View mView;
+    SupportMapFragment mapFragment;
 
     private AroundmeViewModel aroundmeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_aroundme, container, false);
+        mView = inflater.inflate(R.layout.fragment_aroundme, container, false);
+        mapFragment=(SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        if(mapFragment==null){
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            mapFragment = SupportMapFragment.newInstance();
+            ft.replace(R.id.map, mapFragment).commit();
+        }
         /*
         aroundmeViewModel =
                 ViewModelProviders.of(this).get(AroundmeViewModel.class);
@@ -33,7 +55,31 @@ public class AroundmeFragment extends Fragment {
             }
         });
         */
-        return root;
+        mapFragment.getMapAsync(this);
+        return mView;
+
+    }
+
+    /*@Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMapView = (MapView) mView.findViewById(R.id.map);
+        if(mMapView != null){
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }*/
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
 }
